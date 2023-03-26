e630190710912c2edb2c76d86af7daede93416a5
remceTkedaR <radek69tecmer@gmail.com>
2023-03-26 01:45:14 +0100

    editing the comments in English

--- analogCalculatro.py
+++ analogCalculatro.py
@@ -1,10 +1,16 @@
 # Kalkulatro saklowania wartosci analogowych procesowe ( 0-27648)
-#
+# Process analog value scaling calculator
 # przykładowo sygnał procesowy 0-27648 skalujemy do zakresu 0-100%
+# for example, the process signal 0-27648 is scaled to the range of 0-100%
 # wartość zmiennej max 100%
+# variable value max 100%
 # wartość min zmiennej 0%
+# minimum value of the variable 0%
 # max wartość procesowa 27648
+# max process value 27648
 # min wartość procesowa 0
+# min process value 0
+# --------------------------------------
 # y = (y1-y0)/(x1-x0) * (x - x0) + y0
 # --------------------------------------
 
@@ -13,17 +19,18 @@ import math
 value_out = 0.0
 y_out = 0.0
 x_value = 0.0
-x1_value = 27648  # max wartość procesowa sygnału
-x0_value = 0  # min wartość procesowa sygnału
+x1_value = 27648  # max wartość procesowa sygnału / signal process value
+x0_value = 0  # min wartość procesowa sygnału /wartość procesowa sygnału
 y1_value = 100.0
 y0_value = 0.0
 
 
-# y1_value = 0.0  # max wartość zmiennej
-#  y0_value = 0.0  # min wartość zmiennej
+# y1_value = 0.0  # max wartość zmiennej / variable value
+#  y0_value = 0.0  # min wartość zmiennej / variable value
 
 
 def calculation_analog(x1_value, x0_value, y1_value, y0_value):
+    # enter a percentage value in the range 27648 - 0
     x_value = input("podaj wartośc procesową z zakresu 27648 - 0 : ")
     y_out = (y1_value - y0_value) / (x1_value - x0_value) * (int(x_value) - x0_value) + y0_value
     return y_out
