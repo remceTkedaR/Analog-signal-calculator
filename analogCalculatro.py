227fe6e04a72bd7e44326b0712c77a52fbc72fa8
remceTkedaR <radek69tecmer@gmail.com>
2023-03-27 22:06:19 +0200

    editing coments

--- analogCalculatro.py
+++ analogCalculatro.py
@@ -14,28 +14,31 @@
 # y = (y1-y0)/(x1-x0) * (x - x0) + y0
 # --------------------------------------
 
-import math
 
-value_out = 0.0
-y_out = 0.0
-x_value = 0.0
+""""
+#value_out = 0.0
+#y_out = 0.0
+#x_value = 0.0
 x1_value = 27648  # max wartość procesowa sygnału / signal process value
-x0_value = 0  # min wartość procesowa sygnału /wartość procesowa sygnału
+x0_value = 0  # min wartość procesowa sygnału / signal process value
 y1_value = 100.0
 y0_value = 0.0
-
+"""
 
 # y1_value = 0.0  # max wartość zmiennej / variable value
 #  y0_value = 0.0  # min wartość zmiennej / variable value
 
 
 def calculation_analog(x1_value, x0_value, y1_value, y0_value):
+
     # enter a percentage value in the range 27648 - 0
-    x_value = input("podaj wartośc procesową z zakresu 27648 - 0 : ")
-    y_out = (y1_value - y0_value) / (x1_value - x0_value) * (int(x_value) - x0_value) + y0_value
+    x_value = input("podaj wartość procesową z zakresu 27648 - 0 : ")
+    x1_value = 27648
+    x0_value = 0
+    y_out: float = (y1_value - y0_value) / (x1_value - x0_value) * (int(x_value) - x0_value) + y0_value
     return y_out
 
 
-value_out = calculation_analog(27648, 0, 100, 0)
+value_out: float = calculation_analog(27648, 0, 100, 0)
 
 print(value_out)
